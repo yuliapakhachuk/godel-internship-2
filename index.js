fe6e70529913081180d79d8919a4208fedfb9295
Yulia Pakhachuk <yulia.pakhachuk@gmail.com>
2022-05-31 18:51:25 +0200

    adds color change mode script

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-console.log("Я туть");
-const Swiper = require("swiper");
-console.log(Swiper);
+// console.log("Я туть");
+// const Swiper = require("swiper");
+// console.log(Swiper);
+
+const body = document.querySelector("body");
+
+const currentTime = new Date();
+const currentHour = currentTime.getHours();
+
+
+function turnOnLightMode() { 
+    body.setAttribute("style",
+        "--font-color1:#535763; --font-color2:#9BA2BC; --accent-color1:#6759FF; --bg-color1:#F9F9FC; --border-color:#C3C7D9; --header-bg-color:#F9F9FC; --font-color-footer:#F9F9FC");
+}
+
+function turnOnDarkMode() { 
+    body.removeAttribute("style");
+}
+
+function modeSwitcher() {
+    (currentHour > 6 && currentHour <= 19) ? turnOnLightMode() : turnOnDarkMode();
+}
+
+modeSwitcher();

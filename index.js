e85692ad7402f2425dcb3ead136f38114f66816e
Yulia Pakhachuk <yulia.pakhachuk@gmail.com>
2022-06-01 15:07:31 +0200

    fix problem changing styles without reloading

--- index.js
+++ index.js
@@ -4,21 +4,35 @@
 
 const body = document.querySelector("body");
 
-const currentTime = new Date();
-const currentHour = currentTime.getHours();
-
-
 function turnOnLightMode() { 
     body.setAttribute("style",
-        "--font-color1:#535763; --font-color2:#9BA2BC; --accent-color1:#6759FF; --bg-color1:#F9F9FC; --border-color:#C3C7D9; --header-bg-color:#F9F9FC; --font-color-footer:#F9F9FC");
+    "--font-color1:#535763; --font-color2:#9BA2BC; --accent-color1:#6759FF; --bg-color1:#F9F9FC; --border-color:#C3C7D9; --header-bg-color:#F9F9FC; --font-color-footer:#F9F9FC");
 }
 
 function turnOnDarkMode() { 
     body.removeAttribute("style");
 }
 
+const timeNight = setTime(19, 0, 0, 0);
+const timeDay = setTime(6, 0, 0, 0);
+
+function setTime(hour, min, sec, mlsec) { 
+    const timeX = new Date();
+        timeX.setHours(hour);
+        timeX.setMinutes(min);
+        timeX.setSeconds(sec);
+        timeX.setMilliseconds(mlsec);
+    return timeX;
+}
+
+const currentTime = new Date();
+
 function modeSwitcher() {
-    (currentHour > 6 && currentHour <= 19) ? turnOnLightMode() : turnOnDarkMode();
+    const currentHour = new Date();
+    (currentHour >= timeDay && currentHour < timeNight) ? turnOnLightMode() : turnOnDarkMode();
 }
 
 modeSwitcher();
+
+setTimeout(() => modeSwitcher(), timeDay - currentTime);
+setTimeout(() => modeSwitcher(), timeNight - currentTime);
